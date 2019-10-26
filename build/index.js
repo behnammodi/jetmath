ff28aa8b4c0bdda4559e900107dfa6beae3e0078
Behnam Mohammadi <itten@live.com>
2019-10-26 23:25:40 +0330

    added tsconfig

---
+++ build/index.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+function percent(a, b) {
+    return (a / b) * 100;
+}
+exports.percent = percent;
+function baseLog(a, b) {
+    return Math.log(b) / Math.log(a);
+}
+exports.baseLog = baseLog;
+//# sourceMappingURL=index.js.map
