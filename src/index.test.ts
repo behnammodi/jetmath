44c6994c4f263e365cdbfc7f7e9a9afb64b4c80c
Behnam Mohammadi <itten@live.com>
2019-10-26 22:43:03 +0330

    added baseLog method

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,15 @@
-import { percent } from "./index";
+import { percent, baseLog } from "./index";
 
 describe("Test all math method", () => {
   test("percent", () => {
     expect(percent(50, 200)).toBe(25);
     expect(percent(200, 50)).toBe(400);
   });
+
+  test("base log", () => {
+    expect(baseLog(2, 8)).toBe(3);
+    expect(baseLog(2, 4)).toBe(2);
+    expect(baseLog(2, 2)).toBe(1);
+    expect(baseLog(5, 625)).toBe(4);    
+  });
 });
-
-
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ function percent(a: number, b: number): number {
   return (a / b) * 100;
 }
 
-export { percent };
+function baseLog(a: number, b: number): number {
+  return Math.log(b) / Math.log(a);
+}
+
+export { percent, baseLog };
