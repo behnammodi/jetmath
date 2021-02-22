cb5f6968088fee867131b7c2025a808989893924
Behnam Mohammadi <itten@live.com>
2021-02-22 15:29:17 +0330

    chore: improve infrastructure

--- index.d.ts
+++ index.d.ts
@@ -6,5 +6,5 @@ declare function percent(a: number, b: number): number;
 declare function baseLog(a: number, b: number): number;
 declare function circleX(radians: number, radius: number): number;
 declare function circleY(radians: number, radius: number): number;
-declare function distance(startPoint: Point, endPoint: Point): any;
+declare function distance(startPoint: Point, endPoint: Point): number;
 export { percent, baseLog, circleX, circleY, distance };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 exports.__esModule = true;
+exports.distance = exports.circleY = exports.circleX = exports.baseLog = exports.percent = void 0;
 function percent(a, b) {
     return (a / b) * 100;
 }
